277ad04ba97331db4c33d635ff3c4eadd3af506c
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-07-25 16:02:36 +0800

    [#0] - Init project and changed config

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -7,21 +7,17 @@ const SET_GITHUB_USER = 'SET_GITHUB_USER'
 
 const state = {
   gitHubUser: null,  // 这个不要修改，这个不要修改，这个不要修改
-  gitHubUsername: 'bingoogolapple',  // 修改成你自己的 GitHub 账号
+  gitHubUsername: 'JimmyLv',  // 修改成你自己的 GitHub 账号
   pageSize: 10,  // 博客列表每页显示多少条博客
-  showQQGroup: true,  // 如果要显示你自己的 QQ 群二维码图片的话这里配置成 true 并且替换「BGAIssueBlog-Web/static/img/qq-group.png」为你自己的 QQ 群二维码图片，否则配置成 false 即可
+  showQQGroup: false,  // 如果要显示你自己的 QQ 群二维码图片的话这里配置成 true 并且替换「BGAIssueBlog-Web/static/img/qq-group.png」为你自己的 QQ 群二维码图片，否则配置成 false 即可
   thirdPartySite: [  // 配置你想在左上角展示的第三方站点信息
     {
       img: 'static/img/github.png',  // 第三方站点图标，存放在「BGAIssueBlog-Web/static/img」目录中
-      url: 'https://github.com/bingoogolapple'  // 第三方站点的 url
+      url: 'https://github.com/JimmyLv'  // 第三方站点的 url
     },
     {
-      img: 'static/img/weibo.png',
-      url: 'http://weibo.com/bingoogol'
-    },
-    {
-      img: 'static/img/git.png',
-      url: 'https://bingoogolapple.gitbooks.io/bgalearningnotes-git/content'
+      img: 'static/img/gavatar.jpg',
+      url: 'https://blog.jimmylv.info'
     }
     // 如果还有其他站点需要显示，继续在这里追加
   ]
